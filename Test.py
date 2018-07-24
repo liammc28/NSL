886f5da0a7f1b9c03ed66afb01e22ec764f32f69
pmcb99 <paulmcbrien10@gmail.com>
2018-07-24 15:44:00 +0100

    added all files

--- Test.py
+++ Test.py
@@ -1,31 +1,5 @@
 
-
-#
-# wb = openpyxl.load_workbook('/home/pmb/NetSpeedLogger/results.xlsx')
-# ws = wb.active
-#
-# tme = time.strftime("%H:%M",time.localtime())
-# date = time.strftime("%b-%d", time.localtime())
-#
-# try:
-#     Speedtest()
-#     res = sp.shell()
-#     dwn = round((res.download / 1000.0 / 1000.0), 2)
-#     up = round((res.upload / 1000.0 / 1000.0), 2)
-#     png = round(res.ping,2)
-#
-#
-# except:
-#     ws.append(date, time, 'FAIL','FAIL','FAIL')
-#     wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
-#     wb.close()
-#     sys.exit()
-#
-# results = (date, tme, dwn, up, png)
-# ws.append(results)
-# wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
-# wb.close()
-
+ 
 
 def mainz():
     import time
@@ -33,6 +7,7 @@ def mainz():
     import sys
     from openpyxl import Workbook
     import random
+    import csv
 
     wb = openpyxl.load_workbook('/home/pmb/NetSpeedLogger/results.xlsx')
     ws = wb.active
@@ -42,7 +17,7 @@ def mainz():
     try:
         import speedtest as sp
     except:
-        fail_tuple = (date, tme, 'FAIL', 'FAIL', 'FAIL')
+        fail_tuple = (date, tme, 0, 0, 0)
         ws.append(fail_tuple)
         wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
         wb.close()
@@ -58,7 +33,7 @@ def mainz():
     except:
         fail_tuple = (date, tme, 'FAIL', 'FAIL', 'FAIL')
         ws.append(fail_tuple)
-        wb.save('Beta.xlsx')
+        wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
         wb.close()
         sys.exit()
 
@@ -66,6 +41,11 @@ def mainz():
     ws.append(results)
     wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
     wb.close()
+    
+    with open('/home/pmb/NetSpeedLogger/Results.csv','a') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(results)
+        csv_file.close()
 
 mainz()
 
