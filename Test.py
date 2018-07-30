bfc9ee7dd7300be2cdfa79b465ca5583f08b6074
liam <li>
2018-07-30 17:57:05 +0100

    Version 0.5

--- Test.py
+++ Test.py
@@ -1,52 +1,97 @@
 
- 
+"""
+Created on Mon Jul 30 15:34:04 2018
 
-def mainz():
-    import time
-    import openpyxl
-    import sys
-    from openpyxl import Workbook
-    import random
-    import csv
+@author: pmb
 
-    wb = openpyxl.load_workbook('/home/pmb/NetSpeedLogger/results.xlsx')
-    ws = wb.active
-    tme = time.strftime("%H:%M", time.gmtime())
-    date = time.strftime("%b-%d", time.gmtime())
+NetSpeedLoggerBeta0.5
+Main Application
 
+Notes:
+    *Cleaned code by using functions so it is more condensed now
+    *New Feature: OS Detection- now the program detects whether it is running
+     on RPi or PC to write to the correct directories
+
+Planned Updates:
+    *Google Sheets Database for online access to sheet format data
+    *Daily upload/email of csv file for analysis 
+    *Github 
+"""
+
+import os
+import csv
+import openpyxl
+import sys
+import time
+import numpy as np
+import datetime as dt
+
+system = os.uname()
+if system[1] == 'pmblaptop':
+    #directory to write xlsx file
+    xlsx_direc = '/home/pmb/NetSpeedLogger/NSLResults/results.xlsx'
+    #directory to write xlsx info to csv
+    csv_direc = '/home/pmb/NetSpeedLogger/NSLResults/Results.csv'
+    #directory for plot format to be converted to so it can be pulled for analysis
+    plotcsv = '/home/pmb/NetSpeedLogger/NSLResults/ResultsPulled.csv'
+elif system[1] == 'raspberrypi':
+    #directory to write xlsx file
+    xlsx_direc = '/home/pi/NetSpeedLogger/results.xlsx'
+    #directory to write xlsx info to csv
+    csv_direc = '/home/pi/NetSpeedLogger/ResultsPulled.csv'
+    #directory for plot format to be converted to so it can be pulled for analysis
+    plotcsv = '/home/pi/NetSpeedLogger/ResultsPulled.csv'
+else:
+    print("Not programmed for this device, set up directories")
+    sys.exit(-1)
+
+standard_time = int(time.strftime('%y%m%d%H%M'))
+tme = time.strftime("%H:%M", time.localtime())
+date = time.strftime("%b-%d", time.localtime())
+unix_time = time.time()
+    
+def SpeedTester():
+    #attempt to import speedtest module, will log fail if fail occurs
     try:
         import speedtest as sp
+        print()
     except:
-        fail_tuple = (date, tme, 0, 0, 0)
-        ws.append(fail_tuple)
-        wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
-        wb.close()
-        sys.exit(69)
+        log_to_sheet = [date, tme, 0, 0, 0]
+        log_to_csv = [unix_time, 0, 0, standard_time]
 
     try:
         res = sp.shell()
         dwn = round((res.download / 1000.0 / 1000.0), 2)
         up = round((res.upload / 1000.0 / 1000.0), 2)
         png = round(res.ping, 2)
-
+        log_to_sheet = [date, tme, dwn, up, png]
+        log_to_csv = [unix_time, dwn, up, standard_time]
 
     except:
-        fail_tuple = (date, tme, 'FAIL', 'FAIL', 'FAIL')
-        ws.append(fail_tuple)
-        wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
-        wb.close()
-        sys.exit()
+        log_to_sheet = [date, tme, 0, 0, 0]
+        log_to_csv = [unix_time, 0, 0, standard_time]
+    return {'log_to_csv':log_to_csv, 'log_to_sheet':log_to_sheet}
+        
 
-    results = (date, tme, dwn, up, png)
-    ws.append(results)
-    wb.save('/home/pmb/NetSpeedLogger/results.xlsx')
+
+def Writer(table_line, csv_line):
+    #Excel
+    wb = openpyxl.load_workbook(xlsx_direc)
+    ws = wb.active
+    ws.append(table_line)
+    wb.save(xlsx_direc)
     wb.close()
-    
-    with open('/home/pmb/NetSpeedLogger/Results.csv','a') as csv_file:
+    #CSVFile
+    with open(csv_direc,'a') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',')
-        csv_writer.writerow(results)
+        csv_writer.writerow(csv_line)
         csv_file.close()
+    
 
+def mainz():
+    
+    currentResults = SpeedTester()
+    Writer(currentResults['log_to_sheet'],currentResults['log_to_csv'])
+    
 mainz()
-
 
